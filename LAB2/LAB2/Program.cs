dd01fb7ee1fe03f023dc3a8ff719b9e8f9112cc5
s27888 <l0605@pjwstk.edu.pl>
2024-03-12 15:15:45 +0100

    Add avg

--- LAB2/LAB2/Program.cs
+++ LAB2/LAB2/Program.cs
@@ -1,4 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static double avg(int[] arr)
+{
+    double avg = 0;
+    double sum = 0;
+    int amt = arr.Length;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    avg = sum / amt;
+
+    return avg;
+}
+
+Console.WriteLine("Hello, World!");
 //this is a change 1
 //this is a change 2
 //this is a change 3
+
+int[] test = { 1, 2, 3 };
+
+Console.WriteLine(avg(test));
